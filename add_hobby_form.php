2b88e5ddfa32de84e11da63ce6ef2ddb83c51cd9
SimoneMagliano <77775924+SimoneMagliano@users.noreply.github.com>
2021-05-26 11:57:33 +0200

    Add files via upload

---
+++ add_hobby_form.php
@@ -0,0 +1,22 @@
+<?php
+use maglianosimone\usm\model\HobbyModel;
+use maglianosimone\usm\validator\bootstrap\ValidationFormHelper;
+
+require "./__autoload.php";
+session_start();
+$title = 'Aggiungi Interessi';
+$submit = 'aggiungi nuovo interesse';
+$model = new HobbyModel();
+if($_SERVER['REQUEST_METHOD']==='GET'){
+    list($name,$nameClass,$nameClassMessage,$nameMessage) = ValidationFormHelper::getDefault();
+}
+if($_SERVER['REQUEST_METHOD']==='POST'){
+    $hobby = new HobbyModel($_POST['name']);
+    if ($val->getIsValid()) {
+        $model->create($hobby);
+    }
+}
+if($_SESSION['isLoged']==false){
+    header('location: ./login_user.php');
+}
+?>
--- add_user_form.php
+++ add_user_form.php
@@ -46,22 +46,5 @@ if ($_SERVER['REQUEST_METHOD']==='POST') {
         }
     }
 }
-require "./__autoload.php";
-session_start();
-$title = 'Aggiungi Interessi';
-$submit = 'aggiungi nuovo interesse';
-$model = new HobbyModel();
-if($_SERVER['REQUEST_METHOD']==='GET'){
-    list($name,$nameClass,$nameClassMessage,$nameMessage) = ValidationFormHelper::getDefault();
-}
-if($_SERVER['REQUEST_METHOD']==='POST'){
-    $hobby = new HobbyModel($_POST['name']);
-    if ($val->getIsValid()) {
-        $model->create($hobby);
-    }
-}
-if($_SESSION['isLoged']==false){
-    header('location: ./login_user.php');
-}
-include 'src/view/add_user_view.php';
+
 ?>
